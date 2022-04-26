1688ddcfbd8632df9252b52f18d40da73ea136f1
issey <deniskedrovsky@mail.ru>
2022-04-26 23:14:10 +0500

    Косметика

--- schedule.py
+++ schedule.py
@@ -174,9 +174,9 @@ class ScheduleMod(loader.Module):
         start = await self.sort_time(await self.str_timing(lessonStarts))
         end = await self.sort_time(await self.str_timing(lessonsEnds))
         if start[1][1] < end[1][1]: 
-            answer = f'До начала {start[0][0]} урока: {datetime.strftime(datetime.fromtimestamp(start[1][1]), "%H:%M:%S")}\nУрок начнётся в {str(lessonStarts[start[0][0]])}'
+            answer = f'До начала {start[0][0]} урока: <code>{datetime.strftime(datetime.fromtimestamp(start[1][1]), "%H:%M:%S")}</code>\nУрок начнётся в <code>{str(lessonStarts[start[0][0]])}:00</code>'
         else:
-            answer = f'До конца {end[0][0]} урока: {datetime.strftime(datetime.fromtimestamp(end[1][1]), "%H:%M:%S")}\nУрок начнётся в {str(lessonsEnds[end[0][0]])}'
+            answer = f'До конца {end[0][0]} урока: <code>{datetime.strftime(datetime.fromtimestamp(end[1][1]), "%H:%M:%S")}</code>\nУрок начнётся в: <code>{str(lessonsEnds[end[0][0]])}:00</code>'
         await utils.answer(message, answer)
         
         
